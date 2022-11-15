4b34dcfce635a7a2fa0283e79b7ef5b429f0b0d8
Blaquebull <freakanomicsystems@gmail.com>
2022-11-15 06:53:29 +0200

    add basic operations

---
+++ script.js
@@ -0,0 +1,28 @@
+//basic function for button click 
+function add(num1,num2){
+return num1 + num2;
+}
+
+function subtract(num1,num2){
+    return num1 - num2; 
+}
+
+function divide(num1,num2){
+    return num1 / num2;  
+}
+
+function multiply(num1,num2){
+    return num1 * num2; 
+}
+
+function percentage(num){
+    return num/100;   
+}
+
+function clear(){
+    
+}
+
+function backspace(){
+    
+}
